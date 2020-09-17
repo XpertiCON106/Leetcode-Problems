f7ef92baff1b181ca313a79e0515c203107cada7
XpertiCON106 <tahalil106@gmail.com>
2020-09-17 10:02:50 -0400

    Added solution to detect anagrams.

--- Detect Anagrams/detectAnagrams.py
+++ Detect Anagrams/detectAnagrams.py
@@ -9,12 +9,78 @@ hash maps are allowed.
 Examples:
 “angered”, “enraged” → true
 “salad”, “dallas” → false
+================================================================================
+for this problem, i decided to implement mergesort to sort the first two arrays
+I then compared if both are equal. If equal, then return true, which means
+it is an anagram. If not then, return false, which is not an anagram.
 
+for the mergesort function, i take in an array.
+i divide the array in the middle, L and R array.
+then i mergeSort both of those.
+
+then i have two counters that checks through array L and array R
+
+I basically check through 1 by 1. if L[i] < R[j], i put L[i] in the resulting
+array. Increment i. if L[i] > R[j], put in R[j] and increment j. I do this
+until both arrays are exhausted.
+
+One challenge i had was prematurally ending the merge. This happened when both
+strings are not of equal length. To rememdy this, i make sure, whatever elements
+are left of either array, i fill them back in the resulting array.
+
+Because mergesort is stable, the relative ordering of the elements are
+presereved, So i don't have to worry about any re sorting issues.
+
+So, sorting first array takes O(log n)
+sorting the second takes O(log n)
+
+and we just do a comparison check at the end so, overall O(log n) algorithm
 """
 def are_anagrams(t,s):
     if len(t) != len(s):
         print("Not an anagram")
         return False
 
+    arr1 = [x for x in t]
+    arr2 = [x for x in s]
 
-    return True
+    mergeSort(arr1)
+    mergeSort(arr2)
+
+    if arr1 == arr2:
+        print("Is an anagram")
+        return True
+
+    print("Not an anagram")
+    return False
+
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        mergeSort(L)
+        mergeSort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i+= 1
+            else:
+                arr[k] = R[j]
+                j+= 1
+            k+= 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i+= 1
+            k+= 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j+= 1
+            k+= 1
