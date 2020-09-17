6490098228ac5c6f24ac526e1fd5ee86fed0380b
XpertiCON106 <tahalil106@gmail.com>
2020-09-17 09:24:31 -0400

    Updates on Tests.py

--- Detect Anagrams/Tests.py
+++ Detect Anagrams/Tests.py
@@ -1,18 +1,18 @@
 import unittest
-from solution import commonElements
+from detectAnagrams import are_anagrams
 
 class Tests(unittest.TestCase):
     def setUp(self):
         pass
 
     def test0(self):
-        self.assertEqual(false, are_anagrams("1", "bruh"))
+        self.assertEqual(False, are_anagrams("1", "bruh"))
 
     def test1(self):
-        self.assertEqual(true, are_anagrams("angered", "enraged"))
+        self.assertEqual(True, are_anagrams("angered", "enraged"))
 
     def test2(self):
-        self.assertEqual(false, are_anagrams("salad", "dallas"))
+        self.assertEqual(False, are_anagrams("salad", "dallas"))
 
 
 if __name__ == "__main__":
--- Detect Anagrams/detectAnagrams.py
+++ Detect Anagrams/detectAnagrams.py
@@ -3,7 +3,8 @@ Question:
 Given two strings t and s determine if they are anagrams of each other.
 Anagrams are two strings that use the exact same characters.
 For example “angered” and “enraged” are anagrams of each other.
-However, you cannot use hashing, i.e. no hash tables or hash sets or hash maps are allowed.
+However, you cannot use hashing, i.e. no hash tables or hash sets or
+hash maps are allowed.
 
 Examples:
 “angered”, “enraged” → true
@@ -13,7 +14,7 @@ Examples:
 def are_anagrams(t,s):
     if len(t) != len(s):
         print("Not an anagram")
-        return false
+        return False
 
 
-    return true
+    return True
