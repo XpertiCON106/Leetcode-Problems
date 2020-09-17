5211ff42374b5d7e6798f6cd79f12afb5aa4d054
XpertiCON106 <tahalil106@gmail.com>
2020-09-17 09:11:35 -0400

    Added folders

--- Detect Anagrams/detectAnagrams.py
+++ Detect Anagrams/detectAnagrams.py
@@ -4,6 +4,15 @@ Given two strings t and s determine if they are anagrams of each other.
 Anagrams are two strings that use the exact same characters.
 For example “angered” and “enraged” are anagrams of each other.
 However, you cannot use hashing, i.e. no hash tables or hash sets or hash maps are allowed.
+
+Examples:
+“angered”, “enraged” → true
+“salad”, “dallas” → false
+
 """
 def are_anagrams(t,s):
-    
+    if len(t) != len(s):
+        return false
+
+
+    return true
